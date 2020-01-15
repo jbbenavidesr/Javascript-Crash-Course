200fcf94a7764040eda6a499672a49b78f1fcc11
Juan B Benavides <jbbenavidesr@unal.edu.co>
2020-01-15 17:15:14 -0500

    Prompts and changes.

--- home.js
+++ home.js
@@ -10,4 +10,9 @@ console.log(b)
 var num= 42;
 console.log(num)
 
-document.getElementById('someText').innerHTML = 'Hello world';
+// Manipulate DOM with JavaScript
+/* ... Fancy way of saying  Change HTML 
+            with Javascript */
+
+var age= prompt('What is your age?');
+document.getElementById('someText').innerHTML = age;
