8f8e36b94e9c1247522d971b228d20b8c6cd5d24
Juan B Benavides <jbbenavidesr@unal.edu.co>
2020-01-15 17:06:16 -0500

    Hello World

---
+++ home.js
@@ -0,0 +1,13 @@
+console.log('hello');
+// alert('Hello, this is JuanBer');
+
+
+// Variables 
+
+var b= 'smoothie';
+console.log(b)
+
+var num= 42;
+console.log(num)
+
+document.getElementById('someText').innerHTML = 'Hello world';
